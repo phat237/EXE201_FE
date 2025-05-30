a82a657f11756366203a817c60ede82677a1a5ee
Cduy11 <cduy527@gmail.com>
2025-05-30 14:52:59 +0700

    call api create product

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
 import premiumPackagesReducer from "./slices/preniumPackageSlice";
+import productReducer from "./slices/productSlice"
 // tạo store
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     premiumPackages: premiumPackagesReducer,
+    product: productReducer,
   },
 });
